d2fed4d09bebbd24a3c4779b785164f6a3e7ef48
Ellai <ellaiyarasan@intrack.com.my>
2021-05-17 10:38:56 +0800

    chore: add img resource

---
+++ src/components/StatPreview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StatPreview = () => {
+    return (
+        <div className="card-container">
+            <h1>dfdfdf</h1>
+        </div>
+    );
+};
+
+export default StatPreview;
