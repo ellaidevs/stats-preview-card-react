9ad394164763ae655a107042288d7505d9eb5bad
Ellai <ellaiyarasan@intrack.com.my>
2021-05-19 02:06:42 +0800

    feat: add flexbox on styles

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types';
 const Stat = props => {
     return (
         <div className="stat-container">
-            <h1 className="stat-header">{props.header}</h1>
-            <p className="stat-desc">{props.desc}</p>
+                <h1 className="stat-header">{props.header}</h1>
+                <p className="stat-desc">{props.desc}</p>
         </div>
     );
 };
 
-Stat.propTypes = {
-
-};
+Stat.propTypes = {};
 
 export default Stat;
