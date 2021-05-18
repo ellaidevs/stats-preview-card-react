5e884e86269b880fa86ba60740ec2553a5435166
Ellai <ellaiyarasan@intrack.com.my>
2021-05-18 19:16:52 +0800

    feat: adding stat to StatPreview component

--- src/components/StatPreview.js
+++ src/components/StatPreview.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Stat from "./Stat";
 
 const StatPreview = () => {
     return (
         <div className="card-container">
-            <h1>Card</h1>
+            <div className="card-desc-container">
+                <h1 className="main-heading">Get your <span className="text-accent">insights</span> that help your
+                    business grow.
+                </h1>
+                <p className="card-desc">
+                    Discover the benefits of data analytics and make better decisions regarding revenue, customer
+                    experience, and overall efficiency.
+                </p>
+                <div className="stat">
+                    <Stat header={"10k+"} desc={"COMPANIES"}/>
+                    <Stat header={"314"} desc={"TEMPLATES"}/>
+                    <Stat header={"12M+"} desc={"QUERIES"}/>
+                </div>
+            </div>
         </div>
     );
 };
